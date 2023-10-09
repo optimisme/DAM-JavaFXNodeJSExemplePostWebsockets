5194373665cc340ace695f9a77b9ffc136e58c80
optimisme <optimisme@gmail.com>
2023-10-09 12:28:44 +0200

    .

--- JavaFX/run.ps1
+++ JavaFX/run.ps1
@@ -1,19 +1,49 @@
 # run.ps1
+function Get-LatestVersion {
+    param (
+        [string]$moduleName
+    )
 
-# Change to the directory where the script is located
-Set-Location $PSScriptRoot
+    # Find the JAR files and sort them by version while excluding javadoc and sources
+    $jarFiles = Get-ChildItem -Path "$env:USERPROFILE\.m2\repository\org\openjfx" -Recurse -File |
+                Where-Object { $_.Name -like "${moduleName}-*.jar" -and $_.Name -notmatch "(javadoc|sources)" } |
+                Sort-Object Name -Descending
 
-# Set MAVEN_OPTS environment variable
-$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
+    # Return the latest version's path
+    if ($jarFiles.Count -gt 0) {
+        return $jarFiles[0].FullName
+    } else {
+        return $null
+    }
+}
+
+$fxBasePath = Get-LatestVersion "javafx-base"
+$fxControlsPath = Get-LatestVersion "javafx-controls"
+$fxFxmlPath = Get-LatestVersion "javafx-fxml"
+$fxGraphicsPath = Get-LatestVersion "javafx-graphics"
+
+$fxPath = "$fxBasePath;$fxControlsPath;$fxFxmlPath;$fxGraphicsPath"
+
+if (-not $fxPath) {
+    Write-Host "No es pot trobar el mòdul JavaFX al repositori Maven local."
+    exit 1
+}
+
+# Opcions comunes per a MAVEN_OPTS
+$env:MAVEN_OPTS = "--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $fxPath --add-modules javafx.controls,javafx.fxml,javafx.graphics"
+
+# Resta de l'script
 
 # Check for the first argument and set it as the main class
 $mainClass = $args[0]
 
-Write-Host "Setting MAVEN_OPTS to: $env:MAVEN_OPTS"
-Write-Host "Main Class: $mainClass"
+Write-Output "Setting MAVEN_OPTS to: $MAVEN_OPTS"
+Write-Output "Main Class: $mainClass"
 
-# Execute mvn command with the profile and main class as arguments
-$execArg = "-Dexec.mainClass=" + $mainClass
-Write-Host "Exec args: $execArg"
+# Split the execArg into an array
+$execArgs = @("-PrunMain", "-Dexec.mainClass=$mainClass")
 
-mvn clean test-compile exec:java -PrunMain $execArg
+Write-Output "Exec args: $($execArgs -join ' ')"
+
+# Execute mvn command
+mvn clean test-compile exec:java $execArgs
--- JavaFX/run.sh
+++ JavaFX/run.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# run.sh
+get_latest_version() {
+    local module_name=$1
+    find ~/.m2/repository/org/openjfx -name "${module_name}-*.jar" | grep -vE "javadoc|sources" | sort -Vr | head -n1
+}
 
-# Set MAVEN_OPTS environment variable
+FX_BASE_PATH=$(get_latest_version "javafx-base")
+FX_CONTROLS_PATH=$(get_latest_version "javafx-controls")
+FX_FXML_PATH=$(get_latest_version "javafx-fxml")
+FX_GRAPHICS_PATH=$(get_latest_version "javafx-graphics")
+
+FX_PATH="${FX_BASE_PATH}:${FX_CONTROLS_PATH}:${FX_FXML_PATH}:${FX_GRAPHICS_PATH}"
+
+if [[ -z "$FX_PATH" ]]; then
+    echo "No es pot trobar el mòdul JavaFX al repositori Maven local."
+    exit 1
+fi
+
+# Opcions comunes per a MAVEN_OPTS
+export MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $FX_PATH --add-modules javafx.controls,javafx.fxml,javafx.graphics"
+
+# Opcions específiques per a Darwin
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    export MAVEN_OPTS="-Xdock:icon=./target/classes/icons/iconOSX.png --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    export MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
+    export MAVEN_OPTS="$MAVEN_OPTS -Xdock:icon=./target/classes/icons/iconOSX.png"
 fi
 
 # Resta de l'script
@@ -19,7 +35,7 @@ echo "Main Class: $mainClass"
 
 # Execute mvn command with the profile and main class as arguments
 execArg="-PrunMain -Dexec.mainClass=$mainClass"
-echo "Exec args: $mainClass"
+echo "Exec args: $execArg"
 
 # Execute mvn command
-mvn clean test-compile exec:java $execArg 
+mvn clean test-compile exec:java $execArg
